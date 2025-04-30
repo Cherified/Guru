9bd66085202e015859d878b03f477b323d139ced
Murali Vijayaraghavan <muralivi@google.com>
2025-04-30 15:16:30 -0700

    initial commit with libraries copied from Kami

---
+++ Syntax.v
@@ -0,0 +1,10 @@
+Require Export Bool Ascii String List Psatz PeanoNat.
+Require Export Guru.Lib.Word Guru.Lib.WordProperties.
+
+Global Set Implicit Arguments.
+Global Set Asymmetric Patterns.
+
+Global Open Scope string_scope.
+Global Open Scope nat_scope.
+Global Open Scope list_scope.
+
