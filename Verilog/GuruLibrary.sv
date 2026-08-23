9b503b53fcc90ade6c1e1d5154f30f75518a54b7
Murali Vijayaraghavan <vmurali@csail.mit.edu>
2026-08-23 13:24:41 -0700

    fix out of boudns

--- Verilog/GuruLibrary.sv
+++ Verilog/GuruLibrary.sv
@@ -33,7 +33,7 @@ endclass
 
 module verilog_mem#(parameter n=1, parameter clgn=1, parameter sizeK=1, parameter p=1,
                     parameter init=0, parameter def=0,
-                    parameter logic [n*sizeK-1:0] initVal = 0)(
+                    parameter logic [n*sizeK-1:0] initVal = '0)(
   input logic [p-1:0][clgn-1:0] Rq,
   input logic [p-1:0] RqEn,
   input logic [clgn-1:0] WrIdx,
@@ -50,7 +50,7 @@ module verilog_mem#(parameter n=1, parameter clgn=1, parameter sizeK=1, paramete
     if (init) begin
       if (def) begin
         for(i = 0; i < n; i=i+1) begin
-          mem[i] = 0;
+          mem[i] = '0;
         end
       end else begin
         for(i = 0; i < n; i=i+1) begin
@@ -63,11 +63,11 @@ module verilog_mem#(parameter n=1, parameter clgn=1, parameter sizeK=1, paramete
     RpWire = Rp;
     for (i = 0; i < p; i=i+1) begin
       if (RqEn[i]) begin
-        RpWire[i] = mem[Rq[i]];
+        RpWire[i] = (Rq[i] < n) ? mem[Rq[i]] : '0;
       end
     end
     Rp <= RpWire;
-    if (WrEn) begin
+    if (WrEn && WrIdx < n) begin
       mem[WrIdx] <= WrVal;
     end
   end
